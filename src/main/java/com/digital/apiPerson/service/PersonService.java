cbf7819096f5fde43a57ad7a39ed70086fc03505
Brendo Silva <breendo.01@gmail.com>
2021-07-05 15:42:08 -0300

    recurso de listagem

--- src/main/java/com/digital/apiPerson/service/PersonService.java
+++ src/main/java/com/digital/apiPerson/service/PersonService.java
@@ -8,6 +8,9 @@ import com.digital.apiPerson.repository.PersonRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class PersonService {
@@ -30,5 +33,14 @@ public class PersonService {
                 .builder()
                 .message("Created person " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepositorie.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+
     }
 }
